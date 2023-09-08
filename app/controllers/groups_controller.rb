ba8881396c523eb415980e0e7615277b90c7bd14
MauricioNC <mauricionc19@gmail.com>
2023-09-07 22:39:11 -0600

    Generate link for group
    - Function to generate the link for a new group usign
      SecureRandom library. Calling this class, we can call the uuid method
      that will create a universal unique identifier.
    - Gem 'dotenv-rails' installed to create environment variables.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,16 @@
 class GroupsController < ApplicationController
+  include GroupsHelper
+
   def new
     @group = Group.new
   end
 
   def create
     @group = Group.new(group_params)
-
+    
     respond_to do |format|
       if @group.save
+        generate_link()        
         format.html {}
         format.turbo_stream {}
       else
@@ -16,8 +19,13 @@ class GroupsController < ApplicationController
     end
   end
 
-  private
+  def generate_link
+    @link = Link.create(link: SecureRandom.uuid, linkable: @group)
+    @link = ENV["BASE_URL"] + "/" + @link.link + "/users/new"
+  end
 
+  private
+  
   def group_params
     params.require(:group).permit(:group_name, :description)
   end
