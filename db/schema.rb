74e97763c89fd69da9aca40763d511d365390164
MauricioNC <mauricionc19@gmail.com>
2023-09-07 22:51:03 -0600

    Password removed
    password_digest column was removed from user table because the user
    won't have to create an account to register in a group, just have to
    enter the username, full_name and password.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_05_041357) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_08_044959) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_05_041357) do
     t.string "username", null: false
     t.string "full_name", null: false
     t.string "email", null: false
-    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
