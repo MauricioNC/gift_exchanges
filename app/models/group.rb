acb8f775dfc71403b94b19ef2caeba154f9c0639
MauricioNC <mauricionc19@gmail.com>
2023-09-11 21:19:37 -0600

    feat: Model validations
    Validations added into the User and Group model to fill the required
    fields; username, full_name and email for users and group_name for
    groups.

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   has_many :links, as: :linkable
   has_many :user_groups
   has_many :users, through: :user_groups
+
+  validates :group_name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :links, as: :linkable
   has_many :user_groups
   has_many :groups, through: :user_groups
+
+  validates :username, presence: true
+  validates :full_name, presence: true
+  validates :email, presence: true
 end
